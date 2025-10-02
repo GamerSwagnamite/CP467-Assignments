64ee543d2cf2fb8a93be58ca9d2b8873e01d4ad9
fjördan <nyanrainbows8@gmail.com>
2025-10-02 14:40:26 -0400

    fixed pathing with the os library

--- asmo2810_a1/code/Task 1.py
+++ asmo2810_a1/code/Task 1.py
@@ -10,11 +10,19 @@
 # ----------------------------------------------------------------
 import numpy as np
 import cv2 as cv
+
+# debug
+import os
 # ----------------------------------------------------------------
 # 1. Image Interpolation
 # ----------------------------------------------------------------
 # read image 1
-img1 = cv.imread("img1.tif")
+
+# apparently vscode is struggling to locate the image files so we're doing this the hard way
+script_dir = os.path.dirname(os.path.abspath(__file__))     # get our current directory
+assignment_dir = os.path.dirname(script_dir)                # go up one level to the assignment directory
+image_path = os.path.join(assignment_dir, "images", "img1.tif")     # construct the full path to the image
+img1 = cv.imread(image_path)    # do the read
 assert img1 is not None, "file could not be read, check with os.path.exists()"
 
 # scale image 1 down to 1/16th its size
@@ -26,7 +34,7 @@ resize = cv.resize(img1, None, fx = 0.25, fy = 0.25)
 img1_temp = np.zeros((height, width), dtype=np.uint8)
 
 # nearest neighbor interpolation using OpenCV's built-in function
-resize = cv.resize(img1, img1_temp, fx = 4, fy = 4, interpolation = cv.INTER_NEAREST)
+# resize = cv.resize(img1, img1_temp, fx = 4, fy = 4, interpolation = cv.INTER_NEAREST)
 
 # bilinear interpolation implementation from scratch
 
