46fd8bbf2540bdd383113f4dd5c01027fab85740
fjördan <nyanrainbows8@gmail.com>
2025-10-06 21:52:35 -0400

    histogram equalization implementation

---
+++ asmo2810_a1/code/Task 3.py
@@ -0,0 +1,51 @@
+# ----------------------------------------------------------------
+# CP467 Assignment 1
+# ----------------------------------------------------------------
+# Name: Jordan Asmono
+# ID: 210922810
+# Email: asmo2810@mylaurier.ca
+# Date: 2025-10-01
+# ----------------------------------------------------------------
+# Imports
+# ----------------------------------------------------------------
+import numpy as np
+import cv2 as cv
+import os
+
+# ----------------------------------------------------------------
+# helper functions for imports and exports
+# ----------------------------------------------------------------
+def img_write(image, filename):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    assignment_dir = os.path.dirname(script_dir)
+    image_path = os.path.join(assignment_dir, "output images", filename)
+    cv.imwrite(image_path, image)
+    return
+
+def img_read(filename):
+    script_dir = os.path.dirname(os.path.abspath(__file__))     # get our current directory
+    assignment_dir = os.path.dirname(script_dir)                # go up one level to the assignment directory
+    image_path = os.path.join(assignment_dir, "images", filename)     # construct the full path to the image
+    image = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
+    assert image is not None, "file could not be read, check with os.path.exists()"
+    return image
+
+# ----------------------------------------------------------------
+# 3. Histogram Processing
+# ----------------------------------------------------------------
+# read images
+img3 = img_read("img3.tif")
+img4 = img_read("img4.tif")
+img5 = img_read("img5.tif")
+
+# ----------------------------------------------------------------
+# apply histogram equalization on img3
+# ----------------------------------------------------------------
+img3_equalized = cv.equalizeHist(img3)
+img_write(img3_equalized, "img3_equalized.tif")
+
+# ----------------------------------------------------------------
+# apply histogram specialization on img4
+# ----------------------------------------------------------------
+
+
